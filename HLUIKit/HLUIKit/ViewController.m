5501e6e408f797eb6d0f5ba93ab6b395e2f0c6cb
homerice <wyh900207@126.com>
2017-09-19 11:04:16 +0800

    添加pod支持

---
+++ HLUIKit/HLUIKit/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  HLUIKit
+//
+//  Created by 王源鸿 on 2017/9/19.
+//  Copyright © 2017 王源鸿. All rights reserved.
+//
+
+#import "ViewController.h"
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    [self setBackgroundColor];
+}
+
+- (void)setBackgroundColor {
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
